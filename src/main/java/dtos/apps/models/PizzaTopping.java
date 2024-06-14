91972e83be9001cc5a0cc7dd3e0b7c7f9f98e812
JC <cjoaquinangel@gmail.com>
2024-06-14 11:10:18 -0600

    added auto openapi

--- src/main/java/dtos/apps/models/PizzaTopping.java
+++ src/main/java/dtos/apps/models/PizzaTopping.java
@@ -10,7 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "PizzaTopping")
+@Table(name = "pizza_topping")
 public class PizzaTopping {
     @Id
     @Column(name = "id")
